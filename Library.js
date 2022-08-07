edc7a99483919eecfc278495559eb9ac57c1a691
Ebubejisos <109073147+Ebubejisos@users.noreply.github.com>
2022-08-07 10:00:49 +0100

    added a seperate file for library project

---
+++ Library.js
@@ -0,0 +1,63 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author  = author;
+    this.pages = pages;
+    this.isRead = (isRead) => isRead = true ? "Book completed" : "Not yet read" 
+}
+
+
+function addBookToLibrary(){
+    // we will create a prototype next that allows new Books created to access the addBookToLibrary function using Object.create
+}
+
+addBookToLibrary.prototype.add = function(){
+    myLibrary.push(new Book(this.title, this.author, this.pages, this.read));
+}
+// we will now join the book object to the addBookToLibrary prototype
+
+Book.prototype = Object.create(addBookToLibrary.prototype);
+// we are creating an extra function for new Books just incase we want to see an easy description of the book
+
+Book.prototype.info = function(){
+    return`${this.title} by ${this.author}, ${this.pages} pages`
+}
+
+
+// TIME TO CREATE SOME BOOKS AND ADD TO OUR LIBRARY
+new Book("Thor", "Marvel comics", 20).add();
+new Book("Loki", "Kahn the conqueror", 120).add();
+
+// console.log(myLibrary[0].info())
+// console.log(myLibrary)
+
+// VIEW
+// We will create a function that loops through the arrays and displays in the html
+
+
+myLibrary.forEach((element) => {
+    let card = document.createElement("li");
+    card.classList.add("list-group-item");
+    card.innerHTML = element.info();
+    document.getElementById("listEntry").appendChild(card);
+})
+// myLibrary.forEach((element) => console.log(element.info()))
+
+
+// BUTTON CLICK FUNCTION
+document.getElementById('add').onclick = ()=> { 
+        const newDiv = document.createElement("li");
+        newDiv.classList.add("list-group-item");
+        newDiv.innerHTML = document.getElementById("addBook").value;
+        document.getElementById("listEntry").appendChild(newDiv);  
+}
+// background changing onclick
+document.getElementById('btn1').onclick = () => {
+    var body = document.getElementById('html');
+    body.style = 'background: #333a398e'
+}
+document.getElementById('btn2').onclick = () => {
+    var body = document.getElementById('html');
+    body.style = 'background: white'
+}
