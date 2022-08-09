fc68d10e1937fc77003bd476550dbc960771fa01
Ebubejisos <109073147+Ebubejisos@users.noreply.github.com>
2022-08-09 10:00:09 +0100

    edit

--- Library.js
+++ Library.js
@@ -4,14 +4,13 @@ function Book(title, author, pages) {
     this.title = title;
     this.author  = author;
     this.pages = pages;
-    this.isRead = (isRead) => isRead == true ? "Book completed" : "Not yet read" 
 }
 
 // we will create extra functions accessible to each new Book
 
 // Note: prototypes gives new objects reuseable properties or method instead of writing it into the constructor itself that results in duplication of codes. Hence, prototypes save space and makes a program run faster 
 Book.prototype.info = function(){
-    return`${this.title} by ${this.author}, ${this.pages} pages`
+    return`${this.title} by ${this.author}, ${this.pages} pages.`
 }
 Book.prototype.add = function(){
     myLibrary.push(new Book(this.title, this.author, this.pages, this.read));
@@ -29,7 +28,7 @@ new Book("Loki", "Kahn the conqueror", 120).add();
 // VIEW
 // We will create a function that loops through the arrays and displays in the html
 
-
+// manual adding of books from javascript class constructor
 myLibrary.forEach((element) => {
     let card = document.createElement("li");
     card.classList.add("list-group-item");
@@ -40,18 +39,24 @@ myLibrary.forEach((element) => {
 
 
 // BUTTON CLICK FUNCTION
-document.getElementById('add').onclick = ()=> { 
+// appending new book into html library
+document.getElementById('add').onclick = ()=> {
+        const title = document.getElementById("bookTitle").value;
+        const author = document.getElementById("bookAuthor").value;
+        const pages = document.getElementById("bookPages").value;
         const newDiv = document.createElement("li");
         newDiv.classList.add("list-group-item");
-        newDiv.innerHTML = document.getElementById("addBook").value;
-        document.getElementById("listEntry").appendChild(newDiv);  
+        newDiv.innerHTML = `${title} by ${author}, ${pages} pages.`
+        document.getElementById("listEntry").appendChild(newDiv);
 }
-// background changing onclick
-document.getElementById('btn1').onclick = () => {
-    var body = document.getElementById('html');
-    body.style = 'background: #333a398e'
+
+// open form
+function openForm(){
+    let form = document.getElementById("form");
+    form.style.display = "block";
 }
-document.getElementById('btn2').onclick = () => {
-    var body = document.getElementById('html');
-    body.style = 'background: white'
+// close form
+function closeForm(){
+    let form = document.getElementById("form");
+    form.style.display = "none";
 }
