d706e3ce9d8400db2b65ccaceed6a705565a35a9
Ebubejisos <anosikegideon007@gmail.com>
2023-04-13 18:11:51 +0100

    stuck on the function to edit task

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import close from "./images/close.svg";
 
 const Overview = ({ taskArr, handleDelete, handleEdit }) => {
+  const [isEdited, setIsEdited] = useState(false);
+
   return (
     <ul className="overview" style={{ listStyleType: "none" }}>
       {taskArr.map((element) => (
         <li className="task-list" key={element.id}>
           <h2>
             {taskArr.indexOf(element) + 1 + ". "}
-            {element.title}
+            <span
+              id="focus"
+              className="span-text"
+              contentEditable={isEdited ? true : false}
+            >
+              {" "}
+              {element.title}
+            </span>
           </h2>
           <p>
-            {element.details} {""}
+            <span
+              className="span-text"
+              contentEditable={isEdited ? true : false}
+            >
+              {" "}
+              {element.details} {""}
+            </span>
             <img
               src={close}
               alt="close"
@@ -18,7 +34,20 @@ const Overview = ({ taskArr, handleDelete, handleEdit }) => {
               title="remove task"
             />
           </p>
-          <button onClick={() => handleEdit(element.id)}>edit</button>
+          <button
+            onClick={() => {
+              if (isEdited == false) {
+                setIsEdited(true);
+                const span = document.querySelector("#focus");
+                console.log(span.innerText);
+              } else {
+                handleEdit(element.id);
+                setIsEdited(false);
+              }
+            }}
+          >
+            {isEdited == false ? "edit" : "update"}
+          </button>
         </li>
       ))}
     </ul>
